3b611a3ebc011b26621da5ccb2a23ac013f7154f
EzePor <ezeporche@gmail.com>
2024-11-21 13:59:17 -0300

    actualizacion back

--- obj/Debug/net8.0/backNegocio.AssemblyInfo.cs
+++ obj/Debug/net8.0/backNegocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backNegocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654a688e5a8e7c75d7ae99e1ab81d02b6be7abaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a7b1e93915ed7daa81fee5b6c97a0a7d6550d55")]
 [assembly: System.Reflection.AssemblyProductAttribute("backNegocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backNegocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
